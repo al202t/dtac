981ef62191a4a98af43ce4ff1026f042727f81f0
aliasgar1978 <aholo2000@gmail.com>
2025-02-18 10:11:30 +0530

    .

--- dtac_scripts/flexpro_pre_capture/__init__.py
+++ dtac_scripts/flexpro_pre_capture/__init__.py
@@ -5,6 +5,3 @@
 from .flex_connect import FlxConnectCapture
 from .identify_pollers import ActionPollers
 from .common import pull_variables, pull_cmds_lists_dict, print_banner, FILE_VERSION
-
-__version__ = FILE_VERSION
-
