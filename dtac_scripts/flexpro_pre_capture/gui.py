c524c7be2bc2a1746e036e7dfdfc4caed10523a3
aliasgar1978 <aholo2000@gmail.com>
2025-02-20 08:42:14 +0530

    .

--- dtac_scripts/flexpro_pre_capture/gui.py
+++ dtac_scripts/flexpro_pre_capture/gui.py
@@ -16,6 +16,12 @@ from .common import pull_variables, pull_cmds_lists_dict
 #  Static 
 # -----------------------------------------------------------------------------------
 OUTPUT_FOLDER = 'C:/NFV-PreCheck'
+POLLERS = [
+	'rlpv12149.gcsc.att.com',
+	'rlpv12150.gcsc.att.com',
+	'rlpv12151.gcsc.att.com',
+	'rlpv12152.gcsc.att.com',
+]
 
 # -----------------------------------------------------------------------------------
 #  Define all your frames here 
@@ -28,7 +34,7 @@ def dtac_pre_capture():
 	], pad=0)
 	pollers_col = sg.Column([
 		[sg.Text("Pollers(s) List", text_color="black"),], 
-		[sg.Multiline("", key='pc_pollers_list', autoscroll=True, size=(30,6), disabled=False),],
+		[sg.Multiline("\n".join(POLLERS), key='pc_pollers_list', autoscroll=True, size=(30,6), disabled=False),],
 	], pad=0)
 	return sg.Frame(title=None, 
 					relief=sg.RELIEF_SUNKEN, 
@@ -113,7 +119,7 @@ FPC_BUTTUN_PALLETE_DIC["btn_grp_precap"] = {'key': 'btn1',  'frames': FPC_FRAMES
 #    second will be [i] item list of object
 # ================================================================================
 
-@activity_finish_popup
+# @activity_finish_popup
 def pc_start_executor(obj, i):
 	try:
 		# -----------------------------------------------------
